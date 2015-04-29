109861bdd089215dabae443b926d072078095304
Ahmad Rasyid Ismail <ahrasis@gmail.com>
2015-04-29 20:47:25 +0800

    Update ip_updater.php

--- ip_updater.php
+++ ip_updater.php
@@ -3,6 +3,11 @@
 Copyright (c) 2015, Ahmad Rasyid Ismail, ahrasis@gmail.com
 Project IP Updater for ispconfig and dynamic ip users.
 Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
 	* Redistributions of source code must retain the above copyright notice,
 	  this list of conditions and the following disclaimer.
 	* Redistributions in binary form must reproduce the above copyright notice,
@@ -10,6 +15,8 @@ Redistribution and use in source and binary forms, with or without modification,
 	  and/or other materials provided with the distribution.
 	* Neither the above name nor the names of its contributors
 	  may be used to endorse or promote products derived from this software without
+	  specific prior written permission.
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
