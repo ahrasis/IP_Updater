615c321d8644315b9370e1be4eb7211ad3b76c9a
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2017-12-10 17:32:04 +0800

    Update ip_updater.sh

--- ip_updater.sh
+++ ip_updater.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:  UNATTENDED DYNAMIC IP AUTO UPDATER FOR ISPConfig
+# Required-Start:  $local_fs $network
+# Required-Stop:  $local_fs
+# Default-Start:  2 3 4 5
+# Default-Stop:  0 1 6
+# Short-Description:  UNATTENDED DYNAMIC IP AUTO UPDATER FOR ISPConfig
+# Description:  Unattended automatic update of dynamic ip for ISPConfig server.
+### END INIT INFO
 if [ ! -f "/usr/local/ispconfig/interface/lib/ip_updater.php" ]
 then
     cd /usr/local/ispconfig/interface/lib
