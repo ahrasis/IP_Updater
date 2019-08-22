f5c58a211efabe8ab57d084fd2bc3d66c1d73763
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2019-08-22 13:57:35 +0800

    Update ip_updater_multi.php
    Update ip_updater_multi.php with major rewrite so there is no need for any domain from any distance server to remotely access (via nsupdate etc) any ISPConfig dns server that is using this plugin as it will automatically check each and every domains in dns_soa in every prescribed time in its cron job.

--- ip_updater_multi.php
+++ ip_updater_multi.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Copyright (c) 2015-2018, Hj Ahmad Rasyid Hj Ismail "ahrasis" ahrasis@gmail.com
+Copyright (c) 2009-2019, Hj Ahmad Rasyid Hj Ismail "ahrasis" ahrasis@gmail.com
 Project IP Updater for debian and ubuntu, ispconfig 3 and dynamic ipv4 ip users.
 BSD3 License. All rights reserved.
 
@@ -33,143 +33,81 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 $sock = fsockopen('www.google.com', 80, $errno, $errstr, 10);
 if (!$sock) {
-        printf("\r\nNo connection to internet! Retry IP Updater again.\r\n, $errstr ($errno)\r\n");
-        require_once 'ip_updater.php';
-        exit();
+	printf("\r\nNo connection to internet! Retry IP Updater again.\r\n, $errstr ($errno)\r\n");
+	require_once 'ip_updater.php';
+	exit();
 }
 
 /*	Get database access by using ispconfig default configuration so no
 	user and its password are disclosed. Exit if its connection failed */
 
 require_once 'config.inc.php';
-$ip_updater = mysqli_connect($conf['db_host'], $conf['db_user'], $conf['db_password'], $conf['db_database']);
-if (mysqli_connect_errno()) {
-        printf("\r\nConnection to ISPConfig database failed!\r\n", mysqli_connect_error());
-        exit();
+$mysqli = new mysqli($conf['db_host'], $conf['db_user'], $conf['db_password'], $conf['db_database']);
+/* check connection */
+if ($mysqli->connect_errno) {
+	printf("\r\nConnection to ISPConfig database in dns server failed!\r\n", $mysqli->connect_error);
+    exit();
 }
 
-/*	Else, it works. Now get public ip from a reliable source.
-	We are using this but you can define your own. But We just
-	need ipv4. So we exit if its filtering failed */
-
-$public_ip = file_get_contents('http://dynamicdns.park-your-domain.com/getip');
-if(!filter_var($public_ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) === true) {
-	printf("\r\nIPV4 for public IP filtering failed! \r\nYou may need to use other ip source. \r\n\r\n");
-	exit();
+/*	Get domains' details (id, origin, serial from dns_soa table*/
+$binds = $app->db->queryAllRecords("SELECT origin FROM dns_soa WHERE active = 'Y'");
+if (!is_array($binds) || empty($binds)) {
+	printf("\r\nNo useful data available in dns_soa table.\r\n");
+    exit();
 }
 
-/*	Else, it's truly ipv4. Now we obtain the server ip,
-	based on server id. Do change your server id accordingly. */
-
-$query_ip = mysqli_query($ip_updater, '
-  SELECT ip_address 
-  FROM server_ip 
-  WHERE server_id =1
-');
-
-list($db_ip) = mysqli_fetch_row($query_ip);
-
-/*	Other than the above ip, we also need soa ip from bind files */
-
-$binds = glob('/etc/bind/pri.*'); 
-foreach ($binds as $bind)
-	$filename[] = $bind;
-$matcher = file_get_contents($filename[0]);
-preg_match('/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/', $matcher, $matched);
-$soa_ip = $matched[0];
-
-/*	If the server public ip matches both, exit. Log is
-	for error only, so we close database connection and restart
-	apache without any logging for now on. However, you may
-	enable it, by uncommenting the log line below this. */
-
-if(($public_ip == $db_ip) && ($public_ip == $soa_ip)) {
-	// printf("\r\nThe server, soa zone files and public ip addresses match. \r\n\r\n");
-	exit();
-}
-
-/* If not, start to change soa zone files with the new ip */ 
-
-if($public_ip != $soa_ip) {
-	foreach ($binds as $bind) {
-		$file = file_get_contents($bind);
-		file_put_contents($bind, preg_replace("/$soa_ip/", "$public_ip", $file));
-	}
-
-	// Warn and update if soa zone files update failed.
-	foreach(file($bind) as $binding=>$b) {
-		if(strpos($b, "$soa_ip")==true) {
-			printf("\r\nSOA zone files updates failed! \r\nZone files updating code may need a fix or update. \r\n\r\n");
-			exit();
-		}
-	}
-}
-
-/* Then, we update our database with the new ip */
-
-if($public_ip != $db_ip) {
-	$update1 = mysqli_query($ip_updater, "
-    UPDATE dns_rr 
-    SET data = replace(data, '$db_ip', '$public_ip')
-    WHERE  server_id =1
-  ");
-  
-	$update2 = mysqli_query($ip_updater, "
-    UPDATE server_ip 
-    SET ip_address = replace(ip_address, '$db_ip', '$public_ip')
-    WHERE server_id =1
-  ");
-
-	// Warn and exit if database update failed.
-	$query_new_ip = mysqli_query($ip_updater, '
-    SELECT ip_address 
-    FROM server_ip 
-    WHERE server_id =1
-  ');
+foreach ($binds as $bind) {
+	// Check if this domain has proper A record or break the loop
+	if (checkdnsrr($bind, 'A')) $ipv4 = gethostbynamel($bind);
+	else break;
 	
-  list($db_new_ip) = mysqli_fetch_row($query_new_ip);
+	// Get the ipv4 of this domain if it has one (dynamic) ip 
+	if (!empty($ipv4[1])) break;
+	$ipv4 = gethostbyname($bind);
 	
-  if ($public_ip != $db_new_ip) {
+	// Now get zone and data from dns_rr table
+	$query_ip = $app->db->queryOneRecord("SELECT zone, data FROM dns_rr WHERE name LIKE '%$bind%' AND type = 'A' AND active = 'Y'");
+	
+	// Process $query_ip to define $zone and $db_ip if its rows are not empty
+	if ($query_ip->num_rows = 0) break;
+	while($row = $query_ip->fetch_assoc()) $zone = $row['zone']; $db_ip = $row['data'];
+	
+	/* Update ip in column data of dns_rr table with new ip if its public ip is different */
+	if ($ipv4 == $db_ip) break;
+	$update_ip = mysqli_query($ip_updater, "UPDATE dns_rr SET data = replace(data, '$db_ip', '$ipv4') WHERE zone = '$zone'");
+	
+	// Check if this domain updated ip is the same as its public ip
+	$query_ip = $app->db->queryOneRecord("SELECT data FROM dns_rr WHERE name LIKE '%$bind%' AND type = 'A'");
+	list($new_ip) = mysqli_fetch_row($query_new_ip);
+	if ($public_ip != $db_new_ip) {
 		printf("\r\nDatabase updates failed! \r\nDatabase updating code may need a fix or update. \r\n\r\n");
-		exit();
+		break();
+	}
+	
+	/*	Now do dns resync so that above changes updated properly. */
+	// Firstly we deal with the serial in dns_rr table
+	$dns_rr = $app->db->queryOneRecord("SELECT id, serial FROM dns_rr WHERE zone = '$zone'");
+	if(!is_array($dns_rr) || empty($dns_rr)) {
+		printf("\r\nThe record in dns_rr table is unusable or empty. \r\n\r\n");
+		break();
+	}
+	foreach($dns_rr as $rec) {
+		$new_serial = $app->validate_dns->increase_serial($rec["serial"]);
+		$app->db->datalogUpdate('dns_rr', "serial = '".$new_serial."'", 'id', $rec['id']);
+	}
+	// Now we deal with the serial in dns_soa table
+	$dns_soa = $app->db->queryOneRecord("SELECT id, serial FROM dns_soa WHERE origin LIKE '%$bind%'");
+	if(!is_array($dns_soa) || empty($dns_soa)) {
+		printf("\r\nThe record in dns_soa table is unusable or empty. \r\n\r\n");
+		break();
+	}
+	foreach($dns_soa as $rec) {
+		$new_serial = $app->validate_dns->increase_serial($rec["serial"]);
+		$app->db->datalogUpdate('dns_soa', "serial = '".$new_serial."'", 'id', $rec['id']);
 	}
 }
 
-/*	Now do dns resync so that above changes updated properly. */
-
-$zones = $app->db->queryAllRecords("SELECT id,origin,serial FROM dns_soa WHERE active = 'Y'");
-if(is_array($zones) && !empty($zones)) {
-	foreach($zones as $zone) {
-		$records = $app->db->queryAllRecords("SELECT id,serial FROM dns_rr WHERE zone = ".$zone['id']." AND active = 'Y'");
-		if(is_array($records)) {
-			foreach($records as $rec) {
-				$new_serial = $app->validate_dns->increase_serial($rec["serial"]);
-				$app->db->datalogUpdate('dns_rr', "serial = '".$new_serial."'", 'id', $rec['id']);
-
-			}
-		}
-		$new_serial = $app->validate_dns->increase_serial($zone["serial"]);
-		$app->db->datalogUpdate('dns_soa', "serial = '".$new_serial."'", 'id', $zone['id']);
-	}
-}
-
-/*	Lastly, congratulations! All updates are successful. Log is
-	for error only, so we close database connection and restart
-	apache without any logging for now on. However, you may
-	enable it, by uncommenting the line below this. */
-
-// printf("\r\nDatabase and SOA zone files updates are successful! \r\n\r\n");
-
-mysqli_close($ip_updater);
-
-/*	You should define your server software to restart if it is not here. */
-
-if (strpos( $_SERVER['SERVER_SOFTWARE'], 'Apache') !== false) 
-	exec('service apache2 restart');
-if (strpos( $_SERVER['SERVER_SOFTWARE'], 'nginx') !== false) 
-	exec('service nginx restart');
-
-/* Comment this out if you want to reboot afterwards */
-// exec('reboot');
+/* close connection */
+$mysqli->close();
 
 ?>
